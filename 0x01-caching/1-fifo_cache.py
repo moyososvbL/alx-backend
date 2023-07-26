175661f664d87757a8da7836f21e505d7eca4d0e
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2023-07-26 23:50:05 +0000

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-""" 1-main """
-FIFOCache = __import__('1-fifo_cache').FIFOCache
+""" Python caching systems """
+from base_caching import BaseCaching
 
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching system """
+    def __init__(self):
+        """ Initialize class instance. """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = sorted(self.cache_data)[0]
+            self.cache_data.pop(discarded_key)
+            print('DISCARD: {}'.format(discarded_key))
+
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key)
